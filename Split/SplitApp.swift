e581265e52094f9ba95403225571b0d98a62f74d
Imran Razak <Imranrazak1@icloud.com>
2021-11-15 17:18:51 +0000

    Initial Commit

---
+++ Split/SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitApp.swift
+//  Split
+//
+//  Created by Imran razak on 15/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
